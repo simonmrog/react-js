5d3bb5041d1361daa8b3b2235c51aac9554faba5
simonmrog <simonmrog@gmail.com>
2021-05-17 16:36:20 -0500

    fixing error in diff times between client and server with getInitialProps

--- functional-components/react-clock/pages/_app.js
+++ functional-components/react-clock/pages/_app.js
@@ -1,17 +1,44 @@
 import React from "react";
 
-// CSS files
-// import "../src/components/DigitalClock.css";
+// Styles
+import "../src/components/DigitalClock.css";
 
 // components
 import DigitalClock from "../src/components/DigitalClock";
 
 class App extends React.Component {
-  render() {
-    return (
-      <DigitalClock />
-    );
+  static async getInitialProps() {
+    return {
+      time: new Date().toISOString(),
+    };
   }
-};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.time,
+    };
+  }
+
+  tick() {
+    this.setState(() => {
+      return {
+        time: new Date().toISOString(),
+      };
+    });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <DigitalClock time={this.state.time} />;
+  }
+}
 
 export default App;
--- functional-components/react-clock/src/components/DigitalClock.js
+++ functional-components/react-clock/src/components/DigitalClock.js
@@ -1,45 +1,13 @@
 import React from "react";
 
-class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      time: new Date().toLocaleTimeString()
-    }
-  }
-
-  tick() {
-    this.setState(() => {
-      return ({
-        time: new Date().toLocaleTimeString()
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+class DigitalClock extends React.Component {
   render() {
     return (
-      <div class="digital-clock">
-        <h1 className="orange">
-          { this.state.time }
-        </h1>
-        
-        <style jsx>{`
-          .orange {
-            color: #f15b2a;
-          }`
-        }
-        </style>
+      <div className="digital-clock">
+        <h1 className="orange">{this.props.time}</h1>
       </div>
     );
   }
-};
+}
 
-export default Hello;
+export default DigitalClock;
